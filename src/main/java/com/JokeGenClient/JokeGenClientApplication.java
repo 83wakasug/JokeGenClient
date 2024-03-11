042e64b324a4d524f419d979424fc5961013bbce
Wakana <83waksug@gafe.molndal.se>
2024-03-11 20:13:28 +0100

    first commit.

---
+++ src/main/java/com/JokeGenClient/JokeGenClientApplication.java
@@ -0,0 +1,13 @@
+package com.JokeGenClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokeGenClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JokeGenClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JokeGenClient/JokeGenClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JokeGenClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeGenClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
